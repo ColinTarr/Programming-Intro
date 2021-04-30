38e9b8733647827a62691a2c7103291c8358e0b7
Tarr_Colin <Tarr_Colin@student.mahoningctc.com>
2021-04-30 13:45:20 -0400

    first commit

---
+++ CodeCademy Projects/Feedster (Jquery Event Handlers)/main.js
@@ -0,0 +1,35 @@
+$(document).ready(() => {
+    // Nav Menu
+    $('.menu').on('mouseenter', () => {
+        $('.nav-menu').show();
+    });
+    $('.nav-menu').on('mouseleave', () => {
+        $('.nav-menu').hide();
+    });
+
+    //Button Hover
+    $('.btn').on('mouseenter', (event) => {
+        $(event.currentTarget).addClass('btn-hover');
+    }).on('mouseleave', () => {
+        $('.btn').on('mouseleave', (event) => {
+            $(event.currentTarget).removeClass('btn-hover')
+        });
+    });
+
+    //Word Count
+    $('.postText').on('keyup', () => {
+        let post = $(event.currentTarget).val();
+        let remaining = 140 - post.length;
+
+        if (remaining <= 0) {
+            $('.wordcount').addClass('red');
+        } else {
+            $('.wordcount').removeClass('red');
+        }
+
+        $('.characters').html(remaining);
+    });
+
+    $('.postText').focus();
+
+});
